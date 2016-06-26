51592581334dca82950e6a687b3bc6643768ea87
etmnk <antilogic@outlook.com>
2016-06-26 22:47:24 +0900

    change model that use connection pool

--- src/kemalblog/models/basemodel.cr
+++ src/kemalblog/models/basemodel.cr
@@ -1,8 +1,9 @@
 class BaseModel
-  #getter? conn : PG::Connection
+  getter? conn : PG::Connection
+
   def initialize
     ENV["DATABASE_URL"] ||= "postgres://preface@localhost:5432/kemalblog_dev"
-    @conn = PG::Connection.new(ENV["DATABASE_URL"])
+    #@conn = PG::Connection.new(ENV["DATABASE_URL"])
     pg = ConnectionPool(PG::Connection).new(capacity: 25, timeout: 0.1) do
       PG.connect(ENV["DATABASE_URL"])
     end
